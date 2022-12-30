cfa314b3f18572a513a00c12a7f575385fdecd6d
18684092 <18684092@students.lincoln.ac.uk>
2022-12-30 12:54:36 +0000

    Placed onto Beast PC

--- src/andy_ass1/scripts/control.py
+++
@@ -1,7 +0,0 @@
-# General control - switch states / modes from open moving
-# to precision control
-
-# state = open - the robot hunts for grape vine
-# state = goal - robot positions to start of vine
-# state = find - robot looks for grape bunches
-# state = home - robot goes home
