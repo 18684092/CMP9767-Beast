1903a20ad20e989a8fc82d21d5653426d860d80a
18684092 <18684092@students.lincoln.ac.uk>
2023-01-04 14:42:36 +0000

    Added edges

--- andy_ass1/scripts/go_grapevine.py
+++ andy_ass1/scripts/go_grapevine.py
@@ -4,7 +4,7 @@ import actionlib
 from topological_navigation_msgs.msg import GotoNodeAction, GotoNodeGoal
 
 # Waypoints
-nodeList = [1, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1]
+nodeList = [1, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 105]
 
 
 if __name__ == '__main__':
